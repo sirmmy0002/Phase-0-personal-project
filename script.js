90c639c3dc95b5b6f56004b64847639f6d0ae682
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-06 11:03:48 +0300

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Hey I'm Samuel a web Developer";
+h2.textContent = "Hey welcome to my website";
+h2.textContent = "In this page will be presenting my new game the shiegame I will be taking your thourgh the game"
 document.querySelector("body").appendChild(h2);
